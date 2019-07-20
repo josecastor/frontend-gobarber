cc0d0e60325e8fdb0a6425974ce8b2087bfff8df
José Castor <junior.castor@gmail.com>
2019-07-20 16:09:22 -0300

    Layouts por páginas

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,6 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
+import AuthLayout from '../pages/_layouts/auth';
+import DefaultLayout from '../pages/_layouts/default';
+
 export default function RouteWrapper({
   component: Component,
   isPrivate,
@@ -9,15 +12,32 @@ export default function RouteWrapper({
 }) {
   const signed = false;
 
+  /**
+   * validando se o usuário não está logado e se a rota é privada
+   * */
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
   }
 
+  /**
+   * se o usuário está logado e não é uma rota privada
+   * ou seja se está logado não faz sentido ir para tela de login ou cadastro
+   */
   if (signed && !isPrivate) {
     return <Redirect to="/dashboard" />;
   }
 
-  return <Route {...rest} component={Component} />;
+  const Layout = signed ? DefaultLayout : AuthLayout;
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
 }
 
 RouteWrapper.propTypes = {
