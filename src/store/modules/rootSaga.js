5277545cf9837eb363bbd6a99f738670b6faa286
José Castor <junior.castor@gmail.com>
2019-07-27 12:20:33 -0300

    Armazenando perfil

--- src/store/modules/rootSaga.js
+++ src/store/modules/rootSaga.js
@@ -1,7 +1,8 @@
 import { all } from 'redux-saga/effects';
 
 import auth from '~/store/modules/auth/sagas';
+import user from '~/store/modules/user/sagas';
 
 export default function* rootSaga() {
-  return yield all([auth]);
+  return yield all([auth, user]);
 }
