b190821e926a35c31d7093c13f4f86e7fbfac8b1
José Castor <junior.castor@gmail.com>
2019-07-30 08:25:12 -0300

    Listando agendamentos

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,12 +1,24 @@
-import React, { useState, useMemo } from 'react';
-import { format, subDays, addDays } from 'date-fns';
+import React, { useState, useMemo, useEffect } from 'react';
+import {
+  format,
+  subDays,
+  addDays,
+  setHours,
+  setMinutes,
+  setSeconds,
+  isBefore,
+  isEqual,
+  parseISO,
+} from 'date-fns';
 import pt from 'date-fns/locale/pt';
+import { utcToZonedTime } from 'date-fns-tz';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import api from '~/services/api';
 
 import { Container, Time } from './styles';
 
 export default function Dashboard() {
+  const [schedule, setSchedule] = useState([]);
   const [date, setDate] = useState(new Date());
 
   const dateFormatted = useMemo(
@@ -22,6 +34,58 @@ export default function Dashboard() {
     setDate(addDays(date, 1));
   }
 
+  const range = [
+    '08:00',
+    '08:30',
+    '09:00',
+    '09:30',
+    '10:00',
+    '10:30',
+    '11:00',
+    '11:30',
+    '12:00',
+    '12:30',
+    '13:00',
+    '13:30',
+    '14:00',
+    '14:30',
+    '15:00',
+    '15:30',
+    '16:00',
+    '16:30',
+    '17:00',
+    '17:30',
+    '18:00',
+    '18:30',
+    '19:00',
+  ];
+
+  useEffect(() => {
+    async function loadSchedule() {
+      const response = await api.get('schedule', { params: { date } });
+
+      const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+      const data = range.map(hour => {
+        const checkDate = setSeconds(
+          setMinutes(setHours(date, hour.split(':')[0]), hour.split(':')[1]),
+          0
+        );
+        const compareDate = utcToZonedTime(checkDate, timezone);
+        return {
+          time: `${hour}h`,
+          past: isBefore(compareDate, new Date()),
+          appointment: response.data.find(a =>
+            isEqual(parseISO(a.date), compareDate)
+          ),
+        };
+      });
+
+      setSchedule(data);
+    }
+    loadSchedule();
+  }, [date, range]);
+
   return (
     <Container>
       <header>
@@ -35,22 +99,14 @@ export default function Dashboard() {
       </header>
 
       <ul>
-        <Time past>
-          <strong>09:00</strong>
-          <span>Castor</span>
-        </Time>
-        <Time available>
-          <strong>09:30</strong>
-          <span>Em aberto</span>
-        </Time>
-        <Time>
-          <strong>10:00</strong>
-          <span>Castor</span>
-        </Time>
-        <Time>
-          <strong>10:30</strong>
-          <span>Castor</span>
-        </Time>
+        {schedule.map(time => (
+          <Time key={time.time} past={time.past} available={!time.appointment}>
+            <strong>{time.time}</strong>
+            <span>
+              {time.appointment ? time.appointment.user.name : 'Em aberto'}
+            </span>
+          </Time>
+        ))}
       </ul>
     </Container>
   );
